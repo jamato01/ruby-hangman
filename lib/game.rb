a2383e11c2df507f89997c7b831d3b5ba8a17e23
jamato01 <jarrett.m.torres@gmail.com>
2025-10-06 10:21:14 -0400

    Assign secret word at start of game

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,19 @@
 class Game
   def initialize(player_name)
     @player = Player.new(player_name)
-    @solution_word = get_solution
+  end
+
+  def start_game
+    @secret_word = create_word
+    puts @secret_word
+  end
+
+  def create_word
+    f = File.open("lib/google-10000-english-no-swears.txt")
+    word_list = []
+    while (word = f.gets)
+      word_list.push(word) if word.length.between?(5, 12)
+    end
+    word_list.sample
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -3,6 +3,6 @@ class Player
   attr_reader :name
 
   def initialize(player_name)
-    self.name = player_name
+    @name = player_name
   end
 end
