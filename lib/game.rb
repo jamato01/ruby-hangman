10652331bff862df36bc23db1eb89611938899bd
jamato01 <jarrett.m.torres@gmail.com>
2025-10-06 10:47:59 -0400

    Add player turn logic

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,13 @@
 class Game
   def initialize(player_name)
     @player = Player.new(player_name)
+    @remaining_guesses = 6
   end
 
   def start_game
     @secret_word = create_word
-    puts @secret_word
+    create_blank_guess
+    player_turn
   end
 
   def create_word
@@ -15,6 +17,26 @@ class Game
     while (word = f.gets)
       word_list.push(word) if word.length.between?(5, 12)
     end
-    word_list.sample
+    word_list.sample.chars
+  end
+
+  def create_blank_guess
+    @guess_board = Array.new(@secret_word.length, "_ ")
+  end
+
+  def player_turn
+    system("clear")
+    puts "#{@remaining_guesses} incorrect guesses left.\n"
+    puts @guess_board.join
+    puts "\nAlright #{@player.name}, guess a letter."
+    player_guess = @player.ask_for_guess
+    check_guess
+    if check_win
+      win_screen
+    elsif check_lose
+      lose_screen
+    else
+      player_turn
+    end
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -4,5 +4,16 @@ class Player
 
   def initialize(player_name)
     @name = player_name
+    @past_guesses = []
+  end
+
+  def ask_for_guess
+    this_guess = ""
+    loop do
+      this_guess = gets.chomp.downcase
+      puts "Invalid guess. Try again." if this_guess.length != 1 || !this_guess.match?(/[a-z]/)
+      puts "You already guessed this. Try again." if @past_guesses.include?(this_guess)
+    end
+    this_guess
   end
 end
